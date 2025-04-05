3a468ee8e826bf6b50d042b0933f25da946abb33
Aaron Cole <145709167+arrow-cole@users.noreply.github.com>
2025-04-05 15:03:33 -0400

    hide robotics

--- app/other-links/page.tsx
+++ app/other-links/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+{/* "use client";
 
 import React from 'react';
 import Link from 'next/link';
@@ -49,3 +49,4 @@ const LinksPage: React.FC = () => {
 };
 
 export default LinksPage;
+*/}
